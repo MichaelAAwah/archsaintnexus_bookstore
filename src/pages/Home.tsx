eded8dcf261df36ef0443005026ddfd3b11fc51d
MichaelAAwah <awahmichael441@gmail.com>
2025-02-18 14:59:13 +0000

    Minor fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -245,7 +245,6 @@ export default function Home() {
         </div>
       
         <div className="grid md:grid-cols-4 gap-20 mt-12">
-                <BookSkeleton />
           {loadingBooks ? (
             <>
               {[1, 2, 3, 4].map((n) => (
